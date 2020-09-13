7c3328e8756e3dbe7eb94d94effcf45ebde11594
Charles Ikulayo <sleektrends7@gmail.com>
2020-09-13 19:55:05 +0200

    Fibonacci method added

--- functionsExercise/src/Fibonnaci.java
+++ functionsExercise/src/Fibonnaci.java
@@ -1,14 +1,14 @@
 public class Fibonnaci {
     public static void main(String[] args) {
-        System.out.println(fibonacci(5));
+        System.out.println(fibonacci(3));
     }
-    public static int fibonacci(int n){
-        if (n == 0){
+    public static int fibonacci(int nthTerm){
+        if (nthTerm == 0){
             return 0;
-        }else if (n == 1){
+        }else if (nthTerm <= 2){
             return 1;
         }else{
-            return fibonacci(n-1) + fibonacci(n-2);
+            return fibonacci(nthTerm-1) + fibonacci(nthTerm-2);
         }
     }
 }
